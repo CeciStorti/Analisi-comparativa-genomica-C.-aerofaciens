d29a0c8171b23fa22608e335daaa50308740120e
Cecilia Storti <c.storti4@campus.unimib.it>
2026-03-27 15:08:45 +0100

    Update ADH_to_csv.py

--- Code/ADH_to_csv.py
+++ Code/ADH_to_csv.py
@@ -1,24 +1,25 @@
-
 import pandas as pd
 
-def create_presence_absence_matrix(input_file, output_file, gene_name):
-    # Leggi il file di input
-    df = pd.read_csv(input_file,sep='\t')
+def create_presence_absence_matrix(input_file, output_file):
 
-    # Crea la colonna del gene con valori 1 o 0 in base alla quantità
-    df[gene_name] = df["pident"].apply(lambda x: 1 if x > 70 else 0)
+    df = pd.read_csv(input_file, sep='\t')
 
-    # Seleziona solo le colonne necessarie (ID e gene_name)
-    result = df[["subject_id", gene_name]]
+    df["presence"] = df["pident"].apply(lambda x: 1 if x > 70 else 0)
 
-    # Salva la matrice in un nuovo file CSV
-    result.to_csv(output_file, index=False)
+    result = df[["genome_id", "query_id", "presence"]]
 
-    print(f"Matrice di presenza/assenza salvata in {output_file}")
+    # Wide format
+    matrix = result.pivot(index="genome_id", columns="query_id", values="presence")
 
-# Esempio di utilizzo
-input_file = "/home/biouserc/ADH_blast/ref_C_a_4/tmp_2.txt"  
-output_file = "/home/biouserc/ADH_blast/ref_C_a_4/ADH_4.csv" 
-gene_name = "ADH_4"  
+    matrix = matrix.fillna(0)
 
-create_presence_absence_matrix(input_file, output_file, gene_name)
+    # 👉 salva in formato Excel
+    matrix.to_excel(output_file)
+
+    print(f"Matrix saved in {output_file}")
+
+
+input_file = "filtered_blast_results.tsv"
+output_file = "presence_ADH.xlsx"
+
+create_presence_absence_matrix(input_file, output_file)
