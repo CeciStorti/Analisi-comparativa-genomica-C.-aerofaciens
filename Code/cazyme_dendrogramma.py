eae2867611b3104a60c8ed48fd322219b0a90206
Cecilia Storti <c.storti4@campus.unimib.it>
2026-03-27 15:18:08 +0100

    Update cazyme_dendrogramma.py

--- Code/cazyme_dendrogramma.py
+++ Code/cazyme_dendrogramma.py
@@ -24,10 +24,10 @@ for file in file_list:
     df['Genome'] = genome_name
     df_list.append(df)
 
-# Combina tutti i DataFrame in un unico DataFrame
+# Combine all DataFrames in one
 df_combined = pd.concat(df_list, ignore_index=True)
 
-# Creiamo una matrice di presenza/assenza 
+# Generate the presence/absence matrix 
 df_relevant = df_combined[['Genome', 'DIAMOND']]
 df_relevant = df_relevant.drop_duplicates()
 df_presence_absence = df_relevant.groupby(['Genome', 'DIAMOND']).size().unstack(fill_value=0)
@@ -39,7 +39,7 @@ df_final
 
 df_final.to_csv('dbcan/presence_absence_matrix.csv')
 
-# CREAZIONE DENDROGRAMMA:
+# DENDROGRAM:
 
 data=pd.read_csv("dbcan/presence_absence_matrix.csv")
 
@@ -48,28 +48,27 @@ distance_matrix = pdist(data, metric='jaccard')
 
 Z = linkage(distance_matrix, method='complete')  
 
-# Calcoliamo i cluster
-threshold = 0.6 * max(Z[:, 2])  # la soglia può essere modificata come meglio si preferisce
+# Cluster calculation
+threshold = 0.6 * max(Z[:, 2])  
 clusters = fcluster(Z, t=threshold, criterion='distance')
 
 
 unique_clusters = np.unique(clusters)
-cluster_colors = ["steelblue", "orange", "orangered", "violet","lightblue","lightgreen","cadetblue"]  # Specificare i colori dei cluster
+cluster_colors = ["steelblue", "orange", "orangered", "violet","lightblue","lightgreen","cadetblue"]  
 color_map = {cluster: cluster_colors[i % len(cluster_colors)] for i, cluster in enumerate(unique_clusters)}
 hierarchy.set_link_color_palette(["steelblue", "orange", "orangered", "violet","lightblue","lightgreen","cadetblue"])
-# Plot del dendrogramma
+# Plot 
 plt.figure(figsize=(9, 6))
 dendro = hierarchy.dendrogram(Z, color_threshold=threshold, above_threshold_color='gray',no_labels=True)
 
-
-# Legenda 
+# Legend
 cluster_sizes = {cluster: list(clusters).count(cluster) for cluster in unique_clusters}
-legend_elements = [Patch(facecolor=color_map[cluster], label=f'Cluster {cluster}: {size} elementi')
+legend_elements = [Patch(facecolor=color_map[cluster], label=f'Cluster {cluster}: {size} elements')
                    for cluster, size in cluster_sizes.items()]
-plt.legend(handles=legend_elements, loc='upper right', title="Cluster e dimensioni")
+plt.legend(handles=legend_elements, loc='upper right', title="Clusters and dimensions")
 
-plt.title('Dendrogramma basato sulla matrice Presenza/Assenza dei Cazymes')
-plt.xlabel('Genomi')
-plt.ylabel('Distanza (Jaccard)')
+plt.title('Dendrogram based on the Cazymes presence/absence matrix')
+plt.xlabel('Genomes')
+plt.ylabel('Distance (Jaccard)')
 plt.show()
 
